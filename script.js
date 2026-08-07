e5ffa103c3018a495d9ede6033897ef15bfbde80
Andrea Prudenzano <playoff-remake-4c@icloud.com>
2026-08-07 23:17:48 +0100

    Stop overriding shared header width

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ if (year) year.textContent = new Date().getFullYear();
 
 const header = document.querySelector(".site-header");
 if (header) {
-  header.style.maxWidth = "none";
-  header.style.margin = "0";
   header.style.borderBottom = "0";
 }
 
